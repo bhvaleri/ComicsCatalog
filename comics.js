ef43910cd37caf430996b65751cd9c25bcb6e70c
Bill <billvaleriote@gmail.com>
2013-03-20 16:08:57 -0700

    Added in issue, author and artist fields. Still need to make it look pretty.

--- comics.js
+++ comics.js
@@ -2,15 +2,31 @@ var Comic = Backbone.Model.extend({
 
   defaults: function () {
     return {
-      title: "empty comic...",
+      title: "Missing title",
+      issue: "Missing issue",
+      author: "Missing author",
+      artist: "Missing artist",
       description: "no description...",
       order: Comics.nextOrder(),
     };
   },
 
   initialize: function () {
+    //Cleaner way of doing this? Doesn't look that great.
     if (!this.get("title")) {
       this.set({"title": this.defaults().title});
+    }
+
+    if (!this.get("issue")) {
+      this.set({"issue": this.defaults().issue});
+    }
+
+    if (!this.get("author")) {
+      this.set({"author": this.defaults().author });
+    }
+
+    if (!this.get("artist")) {
+      this.set({"artist": this.defaults().artist});
     }
 
     if (!this.get("description")) {
@@ -88,12 +104,19 @@ var AppView = Backbone.View.extend({
   el: $("#comicapp"),
 
   events: {
-    "keypress #new-comic": "moveOnEnter",
+    "keypress #new-title": "moveToIssue",
+    "keypress #new-issue": "moveToAuthor",
+    "keypress #new-author": "moveToArtist",
+    "keypress #new-artist": "moveToDescription",
+
     "keypress #new-description": "createOnEnter"
   },
 
   initialize: function () {
-    this.title = this.$("#new-comic");
+    this.title = this.$("#new-title");
+    this.issue = this.$("#new-issue");
+    this.artist = this.$("#new-artist");
+    this.author = this.$("#new-author");
     this.description = this.$("#new-description");
     
     this.listenTo(Comics, 'add', this.addOne);
@@ -109,10 +132,20 @@ var AppView = Backbone.View.extend({
     this.$("#comic-list").append(view.render().el);
   },
 
-  moveOnEnter: function (e) {
+  moveToIssue: function (e) {
     if (e.keyCode != 13) return;
-    if (!this.title.val()) return;
-
+    $('#new-issue').focus();
+  },
+  moveToAuthor: function (e) {
+    if (e.keyCode != 13) return;
+    $('#new-author').focus();
+  },
+  moveToArtist: function (e) {
+    if (e.keyCode != 13) return;
+    $('#new-artist').focus();
+  },
+  moveToDescription: function (e) {
+    if (e.keyCode != 13) return;
     $('#new-description').focus();
   },
 
@@ -120,8 +153,18 @@ var AppView = Backbone.View.extend({
     if (e.keyCode != 13) return;
     if (!this.title.val() && !this.description.val()) return;
 
-    Comics.create({title: this.title.val(), description: this.description.val()});
+    Comics.create({
+      title: this.title.val(),
+      issue: this.issue.val(),
+      author: this.author.val(),
+      artist: this.artist.val(),
+      description: this.description.val()
+    });
+
     this.title.val('');
+    this.issue.val('');
+    this.author.val('');
+    this.artist.val('');
     this.description.val('');
     $("#new-comic").focus();
   },
