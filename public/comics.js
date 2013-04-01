089eeb8563aaf6668586ca6e17ff63f34eb2fce8
Bill <billvaleriote@gmail.com>
2013-04-01 14:48:56 -0700

    Using JQuery UI to make the cover list sortable, also persisting this new sorted order.

--- public/comics.js
+++ public/comics.js
@@ -7,7 +7,6 @@ var Comic = Backbone.Model.extend({
       artist: "Missing artist",
       description: "no description...",
       image: "xmen.jpg",
-      order: Covers.nextOrder(),
     };
   },
 
@@ -37,42 +36,33 @@ var Comic = Backbone.Model.extend({
       this.set({"image": this.defaults().image});
     }
   },
-
-  toggle: function () {
-    this.save({done: !this.get("done")});
-  }
 });
 
 var Cover = Backbone.Model.extend({
-  defaults: function () {
-
-    var defaultTitle = "Missing title";
-    var defaultImage = "xmen.jpg";
-
-
-    return {
-      title: defaultTitle,
-      image: defaultImage, 
-    };
+  defaults: {
+    order: 0,
+    title: 'Missing Title',
+    image: 'xmen.jpg'
   },
-  
-  //is this neccessary, I'm not sure if the defaults being there will do this
-  //anyways...
+
   initialize: function () {
-    if (!this.get("title")) {
-      this.set({"title": this.defaults().title});
+    if (!this.get('title')) {
+      this.set({ 'title': this.defaults.title });
     }
 
-    if (!this.get("image")) {
-      this.set({"image": this.defaults().image});
+    if (!this.get('image')) {
+      this.set({ 'image': this.defaults.image });
     }
 
-    if (!this.get("comic")) {
-      this.set({"comic": this.defaults().comic});
+    if (!this.get('order')) {
+      this.set({ 'order': this.defaults.order });
+    }
+
+    if (!this.get('comic')) {
+      this.set({ 'comic': new Comic() });
     }
   }
 });
-
 
 var CoverList = Backbone.Collection.extend({
 
@@ -80,34 +70,35 @@ var CoverList = Backbone.Collection.extend({
 
   localStorage: new Backbone.LocalStorage("covers-backbone"),
 
-  nextOrder: function () {
-    if (!this.length) return 1;
-    return this.last().get('order') + 1;
+  sort: function () {
+    _.each(this.models, function (m) {
+      m.save();
+    });
   },
 
-  comparator: function (comic) {
-    return comic.get('order');
-  }
+  comparator: 'order'
 });
 
 var Covers = new CoverList;
 
+
 var CoverView = Backbone.View.extend({
-  tagName: "li",
+  tagName: 'li',
 
   template: _.template($('#cover-template').html()),
-
-  events: {
-    "click .cover-image" : "showComic"
-  },
-
-  initialize: function () {
-    this.listenTo(this.model, 'change', this.render);
-  },
 
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },
+
+  events: {
+   "dblclick .cover-image" : "showComic",
+   "drop": "drop"
+  },
+
+  initialize: function () {
+    this.listenTo(this.model, 'change', this.render);
   },
 
   showComic: function () {
@@ -116,7 +107,112 @@ var CoverView = Backbone.View.extend({
     $('body .comic-view').remove()
     $('body').append(view.render().el);
   },
+
+  drop: function (event, index) {
+    this.$el.trigger('update-sort', [this.model, index]);
+  }
 });
+
+var CoverListView = Backbone.View.extend({
+
+  tagName: 'ul',
+  id: 'cover-list',
+  
+  itemView: CoverView,
+
+  _listItems: null,
+  _listIsSyncing: false,
+
+  orderAttr: 'order',
+
+  render: function () {
+    this._listItems = {};
+    this.listenTo(this.collection, 'sync reset', this.listSync );
+    this.listenTo(this.collection, 'add', this.listAdd );
+    this.listenTo(this.collection, 'remove', this.listRemove );
+
+    this.listSync();
+
+    return this;
+  },
+
+  events: {
+    'sortupdate': 'handleSortComplete'
+  },
+
+  handleSortComplete: function () {
+    var oatr = this.orderAttr;
+
+    _.each(this._listItems, function (v) {
+      v.model.set(oatr, v.$el.index());
+    });
+
+    this.collection.sort();
+    this.listSetup();
+    
+  },
+
+  listSetup: function () {
+    var $ods = this.$('li');
+
+    if ($ods.length == 1 ) {
+      if (this.$el.data('ui-sortable')) {
+        this.$el.sortable('destroy');
+      }
+    } else {
+      this.$el.sortable({ containment: 'parent', tolerance: 'pointer' });
+    }
+
+  },
+
+  listSync: function () {
+    var list = this.collection.models;
+    var sortedList = _.sortBy(list, function (model) { return model.attributes.order; });
+    this._listIsSyncing = true;
+
+    _.invoke(this._listItems, 'remove');
+    this._listItems = {};
+
+    _.each(sortedList, _.bind(function (m) {
+      this.listAdd(m);
+    }, this));
+
+    this._listIsSyncing = false;
+
+    this.listSetup();
+  },
+
+  listAdd: function (model) {
+    var v;
+
+    if (!this._listItems[model.cid]) {
+      v = this._listItems[model.cid] = new this.itemView({ model: model });
+      this.$el.append(v.render().$el);
+    }
+
+    if (!this._listIsSyncing) {
+      this.listSetup();
+    }
+  },
+
+  listRemove: function (model) {
+    if (this._listItems[model.cid]) {
+      this._listItems[model.cid].remove();
+      delete this._listItems[model.cid];
+    }
+
+    if (!this._listIsSyncing) {
+      this.listSetup();
+    }
+  },
+
+  remove: function () {
+    _.invoke(this._listItems, 'remove');
+  }
+
+});
+
+var CoversView = new CoverListView({ collection: Covers });
 
 var ComicView = Backbone.View.extend({
   tagName: "div",
@@ -168,12 +264,14 @@ var AppView = Backbone.View.extend({
     this.image = this.$("#new-image");
     this.description = this.$("#new-description");
     
-    this.listenTo(Covers, 'add', this.addOne);
-    this.listenTo(Covers, 'reset', this.addAll);
+    //this.listenTo(Covers, 'add', this.addOne);
+    //this.listenTo(Covers, 'reset', this.addAll);
 
     this.main = $('.cover-container');
 
     Covers.fetch();
+
+    $('#cover-container').append(CoversView.render().$el);
   },
 
   addOne: function (comic) {
